165b4f7dfa2ec91c282e07b46ac07de6dfa95b8d
Jesse Croxford <takeflightsc@gmail.com>
2017-06-26 16:44:02 -0600

    added newMessage and createMessage custom socket events

---
+++ public/js/index.js
@@ -0,0 +1,18 @@
+const socket = io()
+
+socket.on('connect', function() {
+  console.log('connected to server')
+
+  socket.emit('createMessage', {
+    from: 'user', 
+    message: 'a new user connected'
+  })
+})
+
+socket.on('disconnect', function() {
+  console.log('disconnected from server');
+})
+
+socket.on('newMessage', function(newMessage) {
+  console.log('message:', newMessage)
+})
--- server/server.js
+++ server/server.js
@@ -18,8 +18,27 @@ io.on('connection', (socket) => {
   socket.on('disconnect', () => {
     console.log('user disconnected')
   })
+
+  socket.emit('newMessage', {
+    from: 'john',
+    message: 'see you then',
+    createdAt: '123123'
+  })
+
+  socket.on('createMessage', (newMessage) => {
+    console.log(newMessage)
+    newMessage.createdAt = new Date().getTime()
+  })
 })
 
 server.listen(port, () => {
   console.log(`server is listening on port ${port}`)
 })
+
+// challenge
+// newMessage 
+  // should send from, text, and createdAt data
+  // on client, when message is received, console log it
+// createMessage 
+  // client to server. server should emit newmessage to everyone else
+  // client sends from, and text. server should create createdAt data
