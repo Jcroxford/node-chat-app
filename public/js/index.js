8a19cc79d652cde2fd92d1828013b0b01751f2ad
Jesse Croxford <takeflightsc@gmail.com>
2017-06-26 16:51:24 -0600

    added emit broacast on createMessage event

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ const socket = io()
 
 socket.on('connect', function() {
   console.log('connected to server')
-
-  socket.emit('createMessage', {
-    from: 'user', 
-    message: 'a new user connected'
-  })
 })
 
 socket.on('disconnect', function() {
