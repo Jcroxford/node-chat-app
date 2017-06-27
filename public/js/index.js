ffc55b3b97d77fcd6a36600ed73c80a62cf45a11
Jesse Croxford <takeflightsc@gmail.com>
2017-06-27 13:49:27 -0600

    added autoscrolling if user is at the bottom or close to the bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,22 @@
 var socket = io()
 
+function scrollToBottom() {
+  // selectors
+  var messages   = $('#messages')
+  var newMessage = messages.children('li:last-child')
+
+  //heights
+  var clientHeight      = messages.prop('clientHeight')
+  var scrollHeight      = messages.prop('scrollHeight')
+  var scrollTop         = messages.prop('scrollTop')
+  var newMessageHeight  = newMessage.innerHeight()
+  var lastMessageHeight = newMessage.prev().innerHeight()
+
+  if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight)
+  }
+}
+
 socket.on('connect', function() {
   console.log('connected to server')
 })
@@ -18,6 +35,7 @@ socket.on('newMessage', function(newMessage) {
   })
 
   $('#messages').append(html)
+  scrollToBottom()
 
   // old way to render message output using jquery
   // var li = $('<li></li>')
@@ -36,6 +54,7 @@ socket.on('newLocationMessage', function(message) {
   })
 
   $('#messages').append(html)
+  scrollToBottom()
 
   // old way to render message output using jquery
   // var li = $('<li></li>')
