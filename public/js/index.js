a053e60678085126648444be8625781d60d7cbed
Jesse Croxford <takeflightsc@gmail.com>
2017-06-26 19:03:19 -0600

    added html form and display for messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,4 @@
-const socket = io()
+var socket = io()
 
 socket.on('connect', function() {
   console.log('connected to server')
@@ -10,4 +10,19 @@ socket.on('disconnect', function() {
 
 socket.on('newMessage', function(newMessage) {
   console.log('message:', newMessage)
+  var li = $('<li></li>')
+  li.text(`${newMessage.from}: ${newMessage.text}`)
+
+  $('#messages').append(li)
+})
+
+$('#message-form').on('submit', function(event) {
+  event.preventDefault()
+
+  socket.emit('createMessage', {
+    from: 'User',
+    message: $('[name=message]').val()
+  }, function() {
+
+  })
 })
